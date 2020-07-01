f3cfa48ae424a77405275d7fa0c146b533ce3945
yejiCho <goe152@naver.com>
2020-07-01 14:42:31 +0900

    200701 05 영화로딩

--- src/App.js
+++ src/App.js
@@ -1,44 +1,19 @@
 import React from 'react';
 
-
-// function Food(props){
-//   console.log(props);
-//   return <h1>I like { props.fav }</h1>;
-// }
-
-function Food({name, picture}){
-  return (
-  <div>
-  <h1>I like {name}</h1>
-  <img src={picture}/>
-  </div>
-  );
-}
-
-const foodIlike = [
-  {
-      name : 'Kimchi',
-      image : 'https://ucarecdn.com/c1b36cd7-ebeb-469b-bf95-4120c9cd8dc4/-/scale_crop/1280x1280/center/-/quality/normal/-/format/jpeg/kimchi.jpg'
-  },
-  {
-      name : 'Doncasu',
-      image : 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQG0MEbQppeMfRbHchWxV3JMpYUFR44dhCHWZiZNaW5aEQWZiGO&usqp=CAU'
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: [],
+  };
+  componentDidMount(){
+    setTimeout(() => {
+      this.setState({ isLoading: false});
+    },6000);
   }
-];
-
-
-function App() {
-  // return <div className="App"/>;
-  return (
-    <div>
-      
-      {foodIlike.map(dish => (<Food name={dish.name} picture={dish.image}/>))}
-      {/* <Food fav="kimchi"/>
-      <Food fav="ramen"/>
-      <Food fav="samgiopsal"/>
-      <Food fav="chukumi"/> */}
-    </div>
-  );
+  render() {
+    const { isLoading } = this.state;
+    return <div>{isLoading ? 'Loading...':'We are ready'}</div>;
+  }
 }
 
 export default App;
